798354d97a3038b0fbe4972c26952cb96966fb7b
Mohamed Eldesouki <mohamohamed@qf.org.qa>
2018-11-17 22:53:24 +0300

    Remove add_output_layer func and add property self.require_dLdA

--- natasy/neural_network/network.py
+++ natasy/neural_network/network.py
@@ -1,4 +1,3 @@
-import numpy as np
 from .layers import FullyConnectedLayer, OutputLayer, SoftmaxWithXEntropy
 
 
@@ -8,6 +7,7 @@ class NeuralNetwork:
         self.n_classes = n_classes
         self.layers = list()
         self.has_output_layer = False
+        self.require_dLdA = False
 
     def add_layer(self, n_units, *args, **kwargs):
         if self.has_output_layer:
@@ -23,25 +23,9 @@ class NeuralNetwork:
             layer = FullyConnectedLayer(n_units=n_units, n_in=n_units_previous_layer, *args, **kwargs)
         self.layers.append(layer)
 
-    def add_output_layer(self, *args, **kwargs):
-        if self.is_output_layer:
-            raise Exception # TODO: implement Error "OutputLayerExistError"
-
-        if self.n_classes == 2:
-            n_units = 1
-        elif self.n_classes < 2:
-            raise Exception # TODO: This should be an error in the data
-        else:
-            n_units = self.n_classes
-
-        n_units_previous_layer = self.layers[-1].n_units if self.layers else self.n
-        layer = OutputLayer(n_units=n_units, n_in=n_units_previous_layer, *args, **kwargs)
-        self.layers.append(layer)
-        self.layers[-1].output_layer = True
-
     def feedforward(self, X):
         A = X
-        for i, layer in enumerate(self.layers):
+        for layer in self.layers:
             A = layer.calculate_layer_feedforward(A)
         else:
             return A
