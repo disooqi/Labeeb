0fc39d8c85780789d92b60a301f4e2a430309fa3
Mohamed Eldesouki <disooqi@gmail.com>
2020-03-14 14:19:52 -0400

    Update __init__.py

--- natasy/neural_network/__init__.py
+++ natasy/neural_network/__init__.py
@@ -1,2 +1,7 @@
+"""
+This package contains algorithms for well-known activation and initialization functions.
+"""
+
+# bring model classes directly into package namespace, to save some typing
 from .activations import Activation
 from .initializations import Initialization
